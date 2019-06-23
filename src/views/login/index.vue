dfffdc401c547f3b6450d73fd78bbf7137e0655b
zhaoleitao <zhaoleitao@itcast.cn>
2019-06-23 10:26:30 +0800

    用户登录-页面布局

---
+++ src/views/login/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="login-wrap">
+    <div class="login-from-wrap">
+    <div class="login-head"><img src="./logo_index.png" alt=""></div>
+    <div class="login-form">
+        <el-form ref="form" :model="form">
+      <el-form-item>
+        <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+      </el-form-item>
+        <el-form-item>
+        <el-col :span="10"> <el-input v-model="form.code" placeholder="验证码"> </el-input>
+          </el-col>
+        <el-col :span="10"><el-button @click="handSendCode">获取验证码</el-button>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn-login" type="primary" @click="onSubmit">登陆</el-button>
+        </el-form-item>
+    </el-form>
+    </div>
+  </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'AppLogin',
+  data () {
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    handSendCode () {
+      const { mobile } = this.form
+
+      axios({
+        mehtod: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-wrap{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ccc;
+    .login-head{
+      display: flex;
+      justify-content: center;
+      margin-bottom: 20px;
+    }
+    .login-from-wrap{
+      background-color: #fff;
+      padding: 60px;
+      .btn-login{
+        margin-top: 20px;
+        width: 100%;
+      }
+    }
+}
+</style>
